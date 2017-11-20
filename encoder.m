4015e295490d4a03e9cc5175e48c8ededbb8c6ec
kenderak <joseph.kenderak@gmail.com>
2017-11-20 18:42:47 +0100

    Encoder small refactor

--- encoder.m
+++ encoder.m
@@ -2,6 +2,8 @@ function [ coded ] = encoder( binaryInput, codingTechnique )
 switch codingTechnique
     case 'None'
         coded = binaryInput;
+    otherwise
+        coded = binaryInput;
 
 end
 
